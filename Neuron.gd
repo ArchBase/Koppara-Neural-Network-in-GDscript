ac82f1f049b79fe439159421df6dc2a834f57f72
ArchBase <archbase557@gmail.com>
2024-11-09 16:14:41 +0530

    implemented neuron class

---
+++ Neuron.gd
@@ -0,0 +1,43 @@
+class_name Neuron
+
+var config = {
+	"max_weight_random_value": 1.0,
+	"min_weigth_random_value": -1.0,
+	"no_of_weights": 0,
+	"activation":"relu"
+}
+var parameters = {
+	"weight":[],
+	"bias":0
+}
+
+func relu(x):
+	return max(0, x)
+	
+func sigmoid(x):
+	return 1 / (1 + exp(-x))
+
+func run_input(x:Array[int]):
+	pass
+	
+func _init(no_of_weights:int, activation:String):
+	config['activation'] = activation
+	config["no_of_weights"] = no_of_weights
+	for i in range(no_of_weights):
+		parameters['weight'].append(randf_range(config["min_weigth_random_value"], config["max_weight_random_value"]))
+
+func process_x(x:Array[int]):
+	if len(x) > config["no_of_weights"]:
+		print("no of inputs must be greater or equal no of weights")
+		return null
+	var result = 0
+	for i in range(len(x)): # loop to calculate and add weighted sum
+		result += x[i] * parameters['weight'][i]
+	result += parameters['bias']
+	match config['activation']:
+		"relu":
+			result = relu(result)
+		"sig":
+			result = sigmoid(result)
+	return result
+
